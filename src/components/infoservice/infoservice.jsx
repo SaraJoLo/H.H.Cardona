2f1253c9ae69c66ce3de791c07a61072986fb574
SaraJoLo <162146662+SaraJoLo@users.noreply.github.com>
2024-11-19 14:29:37 +0100

    feature services and information routes

---
+++ src/components/infoservice/infoservice.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./infoservice.scss";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
+function InfoService() {
+    return (
+        <section>
+             <div className="foundImg">
+            <img src='../../public/assets/garden/dronetres.webp' alt="foundImg" />
+        </div>
+        <div className="info-container">
+        <ul>
+            <p>INFORMACION</p>
+          <li class="list"><Link to="/">Ubicación <FontAwesomeIcon icon={ faChevronRight} className='arrowButton' /></Link></li>
+          <li class="list"><Link to="/contact">Contacto<FontAwesomeIcon icon={ faChevronRight} className='arrowButton' /></Link></li>
+          <li class="list"><Link to="/">Valoraciones<FontAwesomeIcon icon={ faChevronRight} className='arrowButton' /></Link></li>
+        </ul>
+        </div>
+        
+        <div className="info-container">
+        <ul>
+            <p>SERVICIOS</p>
+          <li class="list"><Link to="/servicios">Servicios<FontAwesomeIcon icon={ faChevronRight} className='arrowButton' /></Link></li>
+          <li class="list"><Link to="/gallery">Galería de fotos<FontAwesomeIcon icon={ faChevronRight} className='arrowButton' /></Link></li>
+          <li class="list"><Link to="/askers">Preguntas frecuentes  <FontAwesomeIcon icon={ faChevronRight} className='arrowButton' /></Link></li>
+        </ul>
+        </div>
+        </section> 
+    );
+}
+export default InfoService;
