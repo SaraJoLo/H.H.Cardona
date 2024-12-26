0fd43fefdb1678ae6c128997af9cc802aabfd786
SaraJoLo <162146662+SaraJoLo@users.noreply.github.com>
2024-12-26 23:19:17 +0100

    responsive

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.scss';
-import { Link } from 'react-router-dom';
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <section class="headerBox">
+    <section className="headerBox">
       <div className="titleBox">
-        <Link to="/" className="titleLink">Holiday Home Cardona</Link>
+        <a href="/" className="titleLink">Holiday Home Cardona</a>
       </div>
-      <div >
-        <ul className="navBar">
-          <li class="navOp"><Link to="/">Inicio</Link></li>
-          <li class="navOp"><Link to="/aboutus">Sobre nosotros</Link></li>
-          <li class="navOp"><Link to="/services">Servicios</Link></li>
-          <li class="navOp"><Link to="/gallery">Galería</Link></li>
-          <li class="navOp"><Link to="/contact">Contacto</Link></li>
+      
+      <button 
+        className="menuButton" 
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+        aria-label="Menu"
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      <div>
+        <ul className={`navBar ${isMenuOpen ? 'active' : ''}`}>
+          <li className="navOp"><a href="/">Inicio</a></li>
+          <li className="navOp"><a href="/aboutus">Sobre nosotros</a></li>
+          <li className="navOp"><a href="/services">Servicios</a></li>
+          <li className="navOp"><a href="/gallery">Galería</a></li>
+          <li className="navOp"><a href="/contact">Contacto</a></li>
         </ul>
       </div>
     </section>
   );
-};
-export default Header; 
+}
+
+export default Header;
